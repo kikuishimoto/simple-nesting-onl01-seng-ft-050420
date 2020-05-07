f096ae3043363594e70812a78b7171a432ac94e8
kiku ishimoto <kikuish@gmail.com>
2020-05-07 22:49:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		programmer_hash[:allan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -121,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 
 end
